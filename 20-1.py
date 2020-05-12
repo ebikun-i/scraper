23d26f945db68e30759a149dce3c5b690ab0bb2d
ebikun-i <ise@web-cs.co.jp>
2020-05-12 13:20:07 +0900

    scraper一部完成

--- 20-1.py
+++ 20-1.py
@@ -5,6 +5,19 @@ class Scraper:
     def __init__(self,site):
         self.site = site
 
-
     def scrape(self):
-        pass
+        r = urllib.request.urlopen(self.site)
+        html = r.read()
+        parser = "html.parser"
+        sp = BeautifulSoup(html,parser)
+        for tag in sp.find_all("a"):
+            url = tag.get("href")
+            if url is None:
+                continue
+            if "articles" in url:
+                print("\n" + url)
+
+news = "https://news.google.com/topstories?hl=ja&gl=JP&ceid=JP:ja"
+Scraper(news).scrape()
+
+
